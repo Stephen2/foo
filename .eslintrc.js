3c7e1101db32ef862ae74effd650c564db586656
Stephen Pittman <stephen@gomerits.com>
2022-02-09 20:33:31 +1100

    make it work

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,15 @@
-require("@rushstack/eslint-patch/modern-module-resolution");
+// This is a workaround for https://github.com/eslint/eslint/issues/3458
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  "extends": "merit/frontend"
-}
+  extends: [ "merit/frontend" ],  // <---- put your profile string here
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname
+  },
+  settings: {
+    react: {
+      "version": "16.9" // <----
+    }
+  }
+};
